bc3ca6b55814d4b356d32cf8f7ab26153cb7683a
unknown <DevHelloThere>
2024-07-10 05:36:56 +0800

    Set main page to products

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(ProductController::class)->group(function () {
-    Route::get('/products', 'index');
+    Route::get('/', 'index');
     Route::get('/product/add', 'create');
 
     Route::post('/product/store', 'store');
